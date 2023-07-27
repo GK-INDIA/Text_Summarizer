a1b50528f3fe4f42c87d75ad20e2342497f4ad4c
Gyan <gyanprakashkonhar@gmail.com>
2023-07-27 10:27:07 +0530

    folder structure created

--- template.py
+++ template.py
@@ -1,6 +1,6 @@
-import os
-from pathlib import path
 import logging
+import os
+from pathlib import Path
 
 logging.basicConfig(level = logging.INFO, format='[%(asctime)s]: %(message)s')
 
@@ -28,6 +28,18 @@ list_of_files= [
 
 ]
 
-for file path in list_of_files:
+for filepath in list_of_files:
     filepath =  Path(filepath)
     filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir}  for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"File already exists: {filepath}")
